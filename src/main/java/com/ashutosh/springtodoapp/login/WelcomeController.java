525ee14fd3210e530dd791424692cab5d6c654f2
Ashutosh Giri <ashutoshgiri249@gmail.com>
2024-01-17 16:57:34 +0530

    added spring security

--- src/main/java/com/ashutosh/springtodoapp/login/WelcomeController.java
+++ src/main/java/com/ashutosh/springtodoapp/login/WelcomeController.java
@@ -2,18 +2,16 @@ package com.ashutosh.springtodoapp.login;
 
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @SessionAttributes("name")
-public class LoginController {
+public class WelcomeController {
 
 
     @RequestMapping(value="/", method = RequestMethod.GET)
---
+++ src/main/java/com/ashutosh/springtodoapp/security/SpringSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.ashutosh.springtodoapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    //LDAP OR Database
+    //InMemorydetails
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager(){
+        Function<String, String> passwordEncoder;
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+                .username("ashutosh")
+                .password("password")
+                .roles("USER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
